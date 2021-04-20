ac5a3603587848b53518896d515e7a6c02cb4a35
Martin Tsang <m4rtin.t@gmail.com>
2021-04-20 11:45:32 -0400

    Animation assets

--- Assets/_Laz/Scripts/Laz/LazModelBehavior.cs
+++ Assets/_Laz/Scripts/Laz/LazModelBehavior.cs
@@ -4,17 +4,31 @@ namespace Laz
 {
     public class LazModelBehavior : MonoBehaviour
     {
+        [SerializeField] private Animator _animator = null;
         private LazMovement _movement;
 
         public void Initialize(LazMovement movement)
         {
             _movement = movement;
+            _movement.OnSpeedChanges += HandleOnSpeedChanges;
         }
 
         private void FixedUpdate()
         {
             if (_movement.GetCurrentDirection.magnitude != 0)
                 transform.rotation = Quaternion.LookRotation(_movement.GetCurrentDirection, Vector3.up);
+        }
+
+        private void HandleOnSpeedChanges(float speed)
+        {
+            if (speed > 0f)
+            {
+                _animator.SetBool("is_gliding", true);
+            }
+            else
+            {
+                _animator.SetBool("is_gliding", false);
+            }
         }
     }
 }
