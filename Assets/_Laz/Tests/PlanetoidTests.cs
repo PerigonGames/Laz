aa5efb1f343be97967481a0fff31ac02e3a492cf
AbelW <unoctium1@gmail.com>
2021-08-16 03:01:10 -0700

    Write initial test to ensure activating all planets sets to win state

--- Assets/_Laz/Tests/PlanetoidTests.cs
+++ Assets/_Laz/Tests/PlanetoidTests.cs
@@ -58,6 +58,33 @@ namespace Tests.Planets
             Assert.AreEqual(mockPlanetoidBehaviour.ResetCount, 1, "Reset should be called once");
         }
 
+        [Test]
+        public void Test_Planetoid_TriggersWin()
+        {
+            //Arrange
+            var numMockPlanets = 5;
+            MockPlanetoidBehaviour[] mockPlanetoids = new MockPlanetoidBehaviour[numMockPlanets];
+            Planetoid[] planetoids = new Planetoid[numMockPlanets];
+            for(int i = 0; i < numMockPlanets; i++)
+            {
+                mockPlanetoids[i] = new MockPlanetoidBehaviour();
+                planetoids[i] = new Planetoid(mockPlanetoids[i]);
+                mockPlanetoids[i].PlanetoidModel = planetoids[i];
+            }
+            var mockStateManager = new MockStateManager();
+            var lazoWrappableManager = new LazoWrappableManager(mockPlanetoids, mockStateManager);
+
+            //Act
+            mockStateManager.SetState(State.Play);
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                planetoids[i].ActivateLazo();
+            }
+
+            //Assert
+            Assert.AreEqual(mockStateManager.GetState(), State.WinGame, "State should be set to win");
+        }
+
     }
 
 }
