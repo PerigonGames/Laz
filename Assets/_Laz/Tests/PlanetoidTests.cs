22f99d283d3c12001602ea93c3662a77c2ed15d9
AbelW <unoctium1@gmail.com>
2021-08-16 03:11:04 -0700

    Add tests for clean up and reset

--- Assets/_Laz/Tests/PlanetoidTests.cs
+++ Assets/_Laz/Tests/PlanetoidTests.cs
@@ -59,7 +59,7 @@ namespace Tests.Planets
         }
 
         [Test]
-        public void Test_Planetoid_TriggersWin()
+        public void Test_Planetoid_WrappableManager_TriggersWin()
         {
             //Arrange
             var numMockPlanets = 5;
@@ -83,6 +83,69 @@ namespace Tests.Planets
 
             //Assert
             Assert.AreEqual(mockStateManager.GetState(), State.WinGame, "State should be set to win");
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                Assert.IsTrue(planetoids[i].IsActivated, $"Planetoid {i} should be activated");
+            }
+        }
+
+        [Test]
+        public void Test_Planetoid_WrappableManager_CleanUp()
+        {
+            // Arrange
+            var numMockPlanets = 5;
+            MockPlanetoidBehaviour[] mockPlanetoids = new MockPlanetoidBehaviour[numMockPlanets];
+            Planetoid[] planetoids = new Planetoid[numMockPlanets];
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                mockPlanetoids[i] = new MockPlanetoidBehaviour();
+                planetoids[i] = new Planetoid(mockPlanetoids[i]);
+                mockPlanetoids[i].PlanetoidModel = planetoids[i];
+            }
+            var mockStateManager = new MockStateManager();
+            var lazoWrappableManager = new LazoWrappableManager(mockPlanetoids, mockStateManager);
+
+            //Act
+            mockStateManager.SetState(State.Play);
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                planetoids[i].ActivateLazo();
+            }
+            lazoWrappableManager.CleanUp();
+
+            //Assert
+            Assert.AreEqual(mockStateManager.GetState(), State.WinGame, "State should be set to win");
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                Assert.IsFalse(planetoids[i].IsActivated, $"Planetoid {i} should not be activated");
+            }
+        }
+
+        [Test]
+        public void Test_Planetoid_WrappableManager_Reset()
+        {
+            //Arrange
+            var numMockPlanets = 5;
+            MockPlanetoidBehaviour[] mockPlanetoids = new MockPlanetoidBehaviour[numMockPlanets];
+            Planetoid[] planetoids = new Planetoid[numMockPlanets];
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                mockPlanetoids[i] = new MockPlanetoidBehaviour();
+                planetoids[i] = new Planetoid(mockPlanetoids[i]);
+                mockPlanetoids[i].PlanetoidModel = planetoids[i];
+            }
+            var mockStateManager = new MockStateManager();
+            var lazoWrappableManager = new LazoWrappableManager(mockPlanetoids, mockStateManager);
+
+            //Act
+            lazoWrappableManager.Reset();
+
+            //Assert
+            for (int i = 0; i < numMockPlanets; i++)
+            {
+                Assert.AreEqual(mockPlanetoids[i].ResetCount, 1, "Reset should be called once for each planet");
+            }
+
         }
 
     }
